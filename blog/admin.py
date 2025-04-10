299d5f64d7925d14b750567f81b77eae897cc3bc
Shay <sleacy@googlemail.com>
2025-04-10 12:04:40 +0100

    Template setup

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 
 # Import the below to allow us to access the models as admins
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ("title", "slug", "status", "created_on")
+    search_fields = ["title", "content"]
+    list_filter = ("status", "created_on")
+    prepopulated_fields = {"slug": ("title",)}
+    summernote_fields = ("content",)
+
 
 # Register your models here.
 
 # Code to allow access to all posts
-admin.site.register(Post)
 admin.site.register(Comment)
