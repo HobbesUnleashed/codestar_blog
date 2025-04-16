8ec0e2280350844c1a4c8dc097f2a5713f75ad56
Shay <sleacy@googlemail.com>
2025-04-16 09:26:13 +0100

    added post_detail view

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("summernote/", include("django_summernote.urls")),
     path("", include("blog.urls"), name="blog-urls"),
